9b6d724e82a46b77fc15353dd55867a47ea04ded
DenisLubchyk <lubchyk1999@mail.ru>
2017-10-09 16:09:28 +0300

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by Denis Lubchyk on 09.10.17.
+//  Copyright © 2017 Denis Lubchyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by Denis Lubchyk on 09.10.17.
+//  Copyright © 2017 Denis Lubchyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
